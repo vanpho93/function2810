0de689664259d988bcdbf71c4b661df67b719724
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-02 17:52:52 +0700

    c.js

---
+++ c.js
@@ -0,0 +1,24 @@
+/*
+    1. Function in cac so chan trong khoang 1 -> 100
+    1. Function in cac so le trong khoang 1 -> 100
+    1. Function in cac so chia 3 du 1 trong khoang 1 -> 100
+    1. Function in cac so chinh phuong trong khoang 1 -> 100
+*/
+
+function inSoChan() {
+
+}
+
+function inSoLe() {
+
+}
+
+function inSoChia3Du1() {
+
+}
+
+function inSoChinhPhuong() {
+    
+}
+
+// console.log(Math.sqrt(4) % 1);

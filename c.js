ebddd2d8d69d26de9bf262888488d37d3840e06f
Van Pho Nguyen <vanpho02@gmail.com>
2017-11-02 18:30:23 +0700

    c.js

--- c.js
+++ c.js
@@ -1,10 +1,3 @@
-/*
-    1. Function in cac so chan trong khoang 1 -> 100
-    1. Function in cac so le trong khoang 1 -> 100
-    1. Function in cac so chia 3 du 1 trong khoang 1 -> 100
-    1. Function in cac so chinh phuong trong khoang 1 -> 100
-*/
-
 function inSoChan() {
     for(let i = 0; i <= 100; i++) {
         const dk = i % 2 === 0;
@@ -33,6 +26,50 @@ function inSoChinhPhuong() {
     }
 }
 
-inSoChan();
+function inSo(checkNumber) {
+    for(let i = 0; i <= 100; i++) {
+        const dk = checkNumber(i); // kiemTraSoChan(i)
+        if (dk) console.log(i);
+    }
+}
 
-// console.log(Math.sqrt(4) % 1);
+inSo(function (n) {
+    return n % 2 === 0;
+});
+
+inSo(function (n) {
+    return n % 2 === 1;
+});
+
+inSo(function (n) {
+    return n % 3 === 1;
+});
+
+inSo(function (n) {
+    return Math.sqrt(n) % 1 === 0;
+});
+
+
+
+
+
+// function inSo(soChia, soDu) { 
+//     for(let i = 0; i <= 100; i++) {
+//         const dk = i % soChia === soDu;
+//         if (dk) console.log(i);
+//     }
+// }
+
+// function inSoChia3Du1() {
+//     for(let i = 0; i <= 100; i++) {
+//         const dk = i % 3 === 1;
+//         if (dk) console.log(i);
+//     }
+// }
+
+// function inSoChinhPhuong() {
+//     for(let i = 0; i <= 100; i++) {
+//         const dk = Math.sqrt(i) % 1 === 0;
+//         if (dk) console.log(i);
+//     }
+// }
